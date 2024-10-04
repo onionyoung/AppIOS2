9350dd9f103daf93337c4a6b5cabc736dcc90adb
jhyoungfcu <lkk50614@yahoo.com.tw>
2024-10-04 01:07:08 -0700

    Initial Commit

---
+++ AppIOS2Tests/AppIOS2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AppIOS2Tests.swift
+//  AppIOS2Tests
+//
+//  Created by ios on 2024/10/4.
+//
+
+import Testing
+@testable import AppIOS2
+
+struct AppIOS2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
